0249541ae88938e4b2b560aedf121a10a0645438
alexis <faserrano22@misena.edu.co>
2025-03-23 20:27:05 -0500

    Primer commit

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ document.addEventListener("DOMContentLoaded", function () {
     // Agrega un evento al hacer clic en cualquier parte de la página
     document.addEventListener("click", playAudio, { once: true });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carrusel = document.querySelector(".carousel");
+    const imagenes = document.querySelectorAll(".carousel img");
+    const totalImagenes = imagenes.length;
+
+    let indice = 0;
+
+    function moverCarrusel() {
+        indice++;
+        
+        if (indice >= totalImagenes) {
+            carrusel.style.transition = "none";  // Elimina la animación para reiniciar sin parpadeo
+            carrusel.style.transform = "translateX(0)";
+            indice = 0; // Reinicia la posición
+        } else {
+            carrusel.style.transition = "transform 1s ease-in-out";
+            carrusel.style.transform = `translateX(-${indice * 100}%)`;
+        }
+    }
+
+    setInterval(moverCarrusel, 4000);
+});
+
+
+
+
+
